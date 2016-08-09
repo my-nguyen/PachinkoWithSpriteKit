4934b7c164f86bf4ff2f4dc5b4ce04cb9fbf8b50
My Nguyen <My@mys-macbook-pro.local>
2016-08-09 16:52:42 -0700

    created 5 bouncers at the bottom of the screen

--- PachinkoWithSpriteKit/GameScene.swift
+++ PachinkoWithSpriteKit/GameScene.swift
@@ -24,15 +24,11 @@ class GameScene: SKScene {
         // the effect is the falling boxes in touchesBegan() will be stopped/contained at the screen bottom
         physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
 
-        /// create a stationary node at the center of the bottom edge which won't budge when collided
-        /// by red balls
-        let bouncer = SKSpriteNode(imageNamed: "bouncer")
-        // position is center horizontally on the bottom edge
-        bouncer.position = CGPoint(x: 512, y: 0)
-        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width/2.0)
-        // set up so the bouncer object won't move when collided
-        bouncer.physicsBody!.dynamic = false
-        addChild(bouncer)
+        makeBouncerAt(CGPoint(x: 0, y: 0))
+        makeBouncerAt(CGPoint(x: 256, y: 0))
+        makeBouncerAt(CGPoint(x: 512, y: 0))
+        makeBouncerAt(CGPoint(x: 768, y: 0))
+        makeBouncerAt(CGPoint(x: 1024, y: 0))
     }
 
     // this method is triggered when the user touches the screen.
@@ -55,5 +51,15 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+
+    // create a stationary node which won't budge when collided by red balls
+    func makeBouncerAt(position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width/2.0)
+        // set up so the bouncer object won't move when collided
+        bouncer.physicsBody!.dynamic = false
+        addChild(bouncer)
     }
 }
